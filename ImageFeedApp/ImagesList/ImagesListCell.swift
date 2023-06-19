2684085ac199d95668cd751bb9f91aabe95e2c73
dayterr <mvdayter@gmail.com>
2023-06-19 03:24:39 +0300

    sending to 1st review

---
+++ ImageFeedApp/ImagesList/ImagesListCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImagesListCell.swift
+//  ImageFeedApp
+//
+//  Created by Ruth Dayter on 19.06.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
+}
