9ca3f8a1c9985c633a0ac315e80158d3ff91ac80
dayterr <mvdayter@gmail.com>
2023-07-09 01:38:29 +0300

    added profile view

---
+++ ImageFeedApp/Profile/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  ImageFeedApp
+//
+//  Created by Ruth Dayter on 09.07.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var avatarImageView: UIImageView!
+    
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var logoutButton: UIButton!
+    @IBOutlet private var loginLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBAction private func didTapLogoutButton() {
+    }
+}
