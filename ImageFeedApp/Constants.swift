6eee8c354279e4fd6a3b7ad1aa3baf846434a956
dayterr <mvdayter@gmail.com>
2023-07-17 14:32:33 +0300

    added constants

---
+++ ImageFeedApp/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeedApp
+//
+//  Created by Ruth Dayter on 17.07.2023.
+//
+
+import Foundation
+
+let AccessKey = "Tm6YzLmF8pToZy6e4xn_u8irsVccrTooVsrNJBvlZA0"
+let SecretKey = "7sAG1fBtWKKw3DaVaWQpkaayTTSruZU3XOL9DSATWaI"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
