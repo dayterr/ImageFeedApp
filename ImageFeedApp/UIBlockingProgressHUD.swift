bc62d8e741db498751a73a9fcd13a6ff02a77785
dayterr <mvdayter@gmail.com>
2023-09-26 21:42:33 +0300

    removed ui race condition

---
+++ ImageFeedApp/UIBlockingProgressHUD.swift
@@ -0,0 +1,26 @@
+//
+//  UIBlockingProgressHUD.swift
+//  ImageFeedApp
+//
+//  Created by Ruth Dayter on 26.09.2023.
+//
+
+import UIKit
+import ProgressHUD
+
+final class UIBlockingProgressHUD {
+    private static var window: UIWindow? {
+        return UIApplication.shared.windows.first
+    }
+    
+    static func show() {
+        window?.isUserInteractionEnabled = false
+        ProgressHUD.show()
+    }
+    
+    static func dismiss() {
+        window?.isUserInteractionEnabled = true
+        ProgressHUD.dismiss()
+    }
+
+}
