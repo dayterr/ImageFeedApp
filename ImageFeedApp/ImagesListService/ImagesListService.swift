c43537c4ac67466f6a3089e618e79612c71f3983
dayterr <mvdayter@gmail.com>
2023-10-23 19:47:12 +0300

    added fetchPhotosNextPage

---
+++ ImageFeedApp/ImagesListService/ImagesListService.swift
@@ -0,0 +1,8 @@
+//
+//  ImagesListService.swift
+//  ImageFeedApp
+//
+//  Created by Ruth Dayter on 23.10.2023.
+//
+
+import Foundation
---
+++ ImageFeedApp/ImagesListService/Photo.swift
@@ -0,0 +1,8 @@
+//
+//  Photo.swift
+//  ImageFeedApp
+//
+//  Created by Ruth Dayter on 23.10.2023.
+//
+
+import Foundation
